d8e7ebdbb436fc844ad45d44179bfd339bbc82b9
Moo <yunl.zheng@gmail.com>
2014-01-04 00:31:33 +0800

    create new scope by  scope: true

--- src/angular-knob.js
+++ src/angular-knob.js
@@ -2,9 +2,13 @@ angular.module('ui.knob', [])
   .directive('knob', function () {
     return {
       restrict: 'EACM',
-      template: '<input value="{{ knob }}">',
+      template: function(elem, attrs){
+
+        return '<input value="{{ knob }}">';
+
+      },
       replace: true,
-      transclude : true,
+      scope: true,
       link: function (scope, elem, attrs) {
 
         scope.knob = scope.$eval(attrs.knobData);
@@ -18,16 +22,12 @@ angular.module('ui.knob', [])
             opts = scope.$eval(attrs.knobOptions);
           }
 
-          var max = 100;
           if(!angular.isUndefined(attrs.knobMax)){
             var max = scope.$eval(attrs.knobMax);
             if(!angular.isUndefined(max)){
 
-              console.log(scope.knob+ '---' + max);
               opts.max = max;
             
-            }else{
-              console.log('max is undefined');
             }
           }
           
