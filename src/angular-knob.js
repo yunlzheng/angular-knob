329989564bfb201a0ae5a8807515626832ca659b
Corey Wysong <cwysong85@gmail.com>
2015-03-05 20:36:21 -0500

    added readonly to directive for dynamic updating

--- src/angular-knob.js
+++ src/angular-knob.js
@@ -1,31 +1,42 @@
 angular.module('ui.knob', []).directive('knob', ['$timeout', function($timeout) {
-    'use strict';
+  'use strict';
 
-    return {
-        restrict: 'EA',
-        replace: true,
-        template: '<input value="{{ knobData }}"/>',
-        scope: {
-            knobData: '=',
-            knobOptions: '&'
-        },
-        link: function($scope, $element) {
-            var knobInit = $scope.knobOptions() || {};
+  return {
+    restrict: 'EA',
+    replace: true,
+    template: '<input value="{{ knobData }}"/>',
+    scope: {
+      knobData: '=',
+      knobOptions: '&',
+      readOnly: '=ngReadonly'
+    },
+    link: function($scope, $element) {
+      var knobInit = $scope.knobOptions() || {};
 
-            knobInit.release = function(newValue) {
-                $timeout(function() {
-                    $scope.knobData = newValue;
-                    $scope.$apply();
-                });
-            };
+      if($scope.readOnly) {
+        knobInit.readOnly = $scope.readOnly;
+      }
 
-            $scope.$watch('knobData', function(newValue, oldValue) {
-                if (newValue != oldValue) {
-                    $($element).val(newValue).change();
-                }
-            });
+      knobInit.release = function(newValue) {
+        $timeout(function() {
+          $scope.knobData = newValue;
+          $scope.$apply();
+        });
+      };
 
-            $($element).val($scope.knobData).knob(knobInit);
+      $scope.$watch('knobData', function(newValue, oldValue) {
+        if (newValue != oldValue) {
+          $($element).val(newValue).change();
         }
-    };
+      });
+
+      $scope.$watch('readOnly', function(){
+        $($element).trigger('configure', {
+          'readOnly': $scope.readOnly
+        });
+      });
+
+      $($element).val($scope.knobData).knob(knobInit);
+    }
+  };
 }]);
