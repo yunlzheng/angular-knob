9ed54a12603447b11b29e94ccc10b28fc888ea11
probalazs <probalazs@nyenyere.eu>
2016-03-06 19:10:54 +0100

    *FIX indentation

--- src/angular-knob.js
+++ src/angular-knob.js
@@ -1,51 +1,51 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	angular
-		.module('ui.knob', [])
-		.directive('knob', knob);
+    angular
+        .module('ui.knob', [])
+        .directive('knob', knob);
 
-	knob.$inject = [
-		'$timeout'
-	];
+    knob.$inject = [
+        '$timeout'
+    ];
 
-	function knob($timeout) {
-		var directive = {
-			template: '<input ng-model="knobData" ng-value="knobData"/>',
-			scope: { knobData: '=', knobOptions: '&' },
-			link: link
-		};
+    function knob($timeout) {
+        var directive = {
+            template: '<input ng-model="knobData" ng-value="knobData"/>',
+            scope: { knobData: '=', knobOptions: '&' },
+            link: link
+        };
 
-		return directive;
+        return directive;
 
         function link(scope, element) {
-			var knobInit = getKnobInit(scope);
+            var knobInit = getKnobInit(scope);
             element
-				.val(scope.knobData)
-				.knob(knobInit);
+                .val(scope.knobData)
+                .knob(knobInit);
 
             scope.$watch('knobData', function(newValue, oldValue) {
-				knobDataChangeEventHandler(newValue, oldValue, element);
-			});
+                knobDataChangeEventHandler(newValue, oldValue, element);
+            });
         }
 
-		function knobDataChangeEventHandler(newValue, oldValue, element) {
-			if (newValue !== oldValue) {
-				element
-					.val(newValue)
-					.change();
-			}
-		}
+        function knobDataChangeEventHandler(newValue, oldValue, element) {
+            if (newValue !== oldValue) {
+                element
+                    .val(newValue)
+                    .change();
+            }
+        }
 
-		function getKnobInit(scope) {
-			var init = scope.knobOptions() || {};
-			init.release = function(newValue) {
-				$timeout(function() {
-					scope.knobData = newValue;
-					scope.$apply();
-				});
-			};
-			return init;
-		}
-	}
+        function getKnobInit(scope) {
+            var init = scope.knobOptions() || {};
+            init.release = function(newValue) {
+                $timeout(function() {
+                    scope.knobData = newValue;
+                    scope.$apply();
+                });
+            };
+            return init;
+        }
+    }
 })();
