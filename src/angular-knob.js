ee12b6c92a7ba669aa3fca34f69a8e311193aa0e
Liu Rui <lovearuis@gmail.com>
2016-03-16 22:37:51 +0800

    调整格式

--- src/angular-knob.js
+++ src/angular-knob.js
@@ -1,33 +1,32 @@
-angular.module('ui.knob', []).directive('knob', ['$timeout', function ($timeout) {
-  'use strict';
+angular.module('ui.knob', []).directive('knob', ['$timeout', function($timeout) {
+    'use strict';
 
-  return {
-    restrict: 'EA',
-    replace: true,
-    template: '<input value="{{ knobData }}"/>',
-    scope: {
-      knobData: '=',
-      knobOptions: '&'
-    },
-    link: function ($scope, $element) {
-      var knobInit = $scope.knobOptions() || {};
-      var _value;
+    return {
+        restrict: 'EA',
+        replace: true,
+        template: '<input value="{{ knobData }}"/>',
+        scope: {
+            knobData: '=',
+            knobOptions: '&'
+        },
+        link: function($scope, $element) {
+            var knobInit = $scope.knobOptions() || {};
+            var _value;
+            knobInit.release = function(newValue) {
+                $timeout(function() {
+                    $scope.knobData = newValue;
+                    _value = newValue;
+                    // $scope.$apply();
+                });
+            };
 
-      knobInit.release = function (newValue) {
-        $timeout(function () {
-          $scope.knobData = newValue;
-          _value = newValue;
-          //$scope.$apply();
-        });
-      };
+            $scope.$watch('knobData', function(newValue, oldValue) {
+                if (newValue != oldValue && newValue != _value) {
+                    $($element).val(newValue).change();
+                }
+            });
 
-      $scope.$watch('knobData', function (newValue, oldValue) {
-        if (newValue != oldValue && newValue != _value) {
-          $($element).val(newValue).change();
+            $($element).val($scope.knobData).knob(knobInit);
         }
-      });
-
-      $($element).val($scope.knobData).knob(knobInit);
-    }
-  };
+    };
 }]);
